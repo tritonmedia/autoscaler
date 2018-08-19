a58501fd22f707aed6247cceff4318a3f288d9ba
Jared Allard <jaredallard@outlook.com>
2018-08-18 18:19:53 -0700

    feat: switch to global scaleUp/Down status system

--- index.js
+++ index.js
@@ -12,14 +12,35 @@ const dyn = require('triton-core/dynamics')
 const Config = require('triton-core/config')
 const kue = require('kue')
 const path = require('path')
-const fecha = require('fecha')
 const events = require('events')
-const pretty = require('pretty-ms')
 const logger = require('pino')({
   name: path.basename(__filename)
 })
 
-const waiting = {}
+const statuses = {
+  isPendingScaleDown: {
+    status: false,
+    since: new Date()
+  },
+  isPendingScaleUp: {
+    status: false,
+    since: new Date()
+  },
+  lastScaleDownTime: new Date(),
+  lastScaleUpTime: new Date()
+}
+
+const printStatus = () => {
+  const scaleDown = statuses.isPendingScaleDown.status
+  const scaleUp = statuses.isPendingScaleUp.status
+
+  const child = logger.child({
+    scaleDown: scaleDown,
+    scaleUp: scaleUp
+  })
+
+  child.info(`autoscale status report`)
+}
 
 const init = async () => {
   const config = await Config('events')
@@ -31,63 +52,84 @@ const init = async () => {
   const kube = await require('./lib/kube')(config)
   const watcher = require('./lib/watcher')
 
-  setInterval(watcher.watcher(queue, emitter), 1000)
+  setInterval(watcher.watcher(queue, emitter), 5000)
 
   // wait a bit to spin up a new deployment
-  setInterval(async function() {
+  setInterval(async function () {
+    const scaleTime = (1000 * 60) * 1
     const now = new Date()
-    const keys = Object.keys(waiting)
 
-    for(const jobId of keys) {
-      const since = waiting[jobId].date
-      const ack = waiting[jobId].ack
+    // scale up
+    if (statuses.isPendingScaleUp.status && !statuses.isPendingScaleDown.status) {
+      const diff = now.valueOf() - statuses.isPendingScaleUp.since.valueOf()
 
-      const child = logger.child({
-        job: jobId,
-        since,
-        ack
-      })
-
-      // noop on ack
-      if (ack) continue 
-
-      const diff = now.valueOf() - since.valueOf()
-
-      const sinceFriendly = fecha.format(since, 'default')
-      child.info(`${jobId} has been pending for ${pretty(diff)}`)
-
-      // 5 minutes
-      if (diff > (1000 * 60) * 1 ) {
-        waiting[jobId].ack = true
-        child.info('triggered scale up')
+      // 1 minute
+      if (diff > scaleTime) {
+        logger.info('triggered scale up')
 
         try {
-          await kube.scaleUp("triton-converter")
-        } catch(err) {
-          child.error('failed to scale up, requeue.')
-          delete waiting[jobId]
+          await kube.scaleUp('triton-converter')
+        } catch (err) {
+          logger.error('failed to scale up, requeue.', err)
         }
       }
     }
+
+    // scale down
+    if (statuses.isPendingScaleDown.status && !statuses.isPendingScaleUp.status) {
+      const diff = now.valueOf() - statuses.isPendingScaleDown.since.valueOf()
+
+      // 1 minute
+      if (diff > scaleTime) {
+        logger.info('triggered scale down')
+
+        try {
+          await kube.scaleDown('triton-converter')
+        } catch (err) {
+          logger.error('failed to scale down, requeue.', err)
+        }
+      }
+    }
+
+    printStatus()
   }, 10000)
 
   // add to the 'waiting' object
-  emitter.on('inactive-jobs', inactive => {
-    logger.debug('inactive-fired', inactive)
+  emitter.on('inactive', inactive => {
+    if (inactive !== 0) {
+      logger.info('there are pending jobs')
 
-    inactive.forEach(job => {
-      if (waiting[job.id] === undefined) {
-        waiting[job.id] = {
-          date: new Date(),
-          ack: false
-        }
+      if (!statuses.isPendingScaleUp.status) {
+        statuses.isPendingScaleUp.status = true
+        statuses.isPendingScaleUp.since = new Date()
       }
-    })
+    } else {
+      logger.debug('canceled scaleUp, if there was one present')
+      statuses.isPendingScaleUp.status = false
+    }
+
+    logger.debug(`inactive=${inactive}`)
   })
 
-  emitter.on('completed-jobs', completed => {
-    logger.debug('completed-fired', completed)
+  emitter.on('active', async active => {
+    // process new stuff
+    const replicas = await kube.getReplicas('triton-converter')
+    if (replicas > active) {
+      logger.debug('there are more replicas than active jobs, scaleDown')
+
+      if (!statuses.isPendingScaleDown.status) {
+        statuses.isPendingScaleDown.status = true
+        statuses.isPendingScaleDown.since = new Date()
+      }
+    } else {
+      logger.debug('canceled scaleDown, if there was one present')
+      statuses.isPendingScaleDown.status = false
+    }
+
+    logger.debug(`replicas=${replicas}, active=${active}`)
   })
+
+  queue.watchStuckJobs()
 
   logger.info('initialized')
 }

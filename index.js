329fe5b01ccb830f286efabd97e99da0728d5bac
Jared Allard <jaredallard@outlook.com>
2019-05-08 05:12:58 -0700

    feat: support for reading from CRD

--- index.js
+++ index.js
@@ -14,13 +14,17 @@ const kue = require('kue')
 const path = require('path')
 const events = require('events')
 const Redis = require('ioredis')
-const Watcher = require('./lib/watcher')
-const JobQueue = require('./lib/job')
+const prettyMs = require('pretty-ms')
 const os = require('os')
 const uuid = require('uuid/v4')
 const logger = require('pino')({
   name: path.basename(__filename)
 })
+
+// internals
+const Watcher = require('./lib/watcher')
+const JobQueue = require('./lib/job')
+const Kube = require('./lib/kube')
 
 // lodash shim to make it easier to understand what's going on here.
 const _ = {
@@ -32,10 +36,12 @@ const printStatus = async () => {
   const pendingJobs = await jobQueue.list()
   for (const op of pendingJobs) {
     const w = watcherTable.get(op.data.watcher)
-    const queue = w.jobType
     const deployment = w.deployment
 
-    logger.info(`Pending Operation: op=${op.data.op},queue=${queue},deployment=${deployment},watcher=${op.data.watcher}`)
+    const now = new Date()
+    const diff = now.valueOf() - op.created_at.valueOf()
+
+    logger.info(`Operation '${op.data.op}' deployment '${deployment}' has been pending for ${prettyMs(diff)} (${op.created_at.toISOString()})`)
   }
 }
 
@@ -48,20 +54,6 @@ const WatcherData = {
   deploymentName: '',
   jobType: ''
 }
-
-/**
- * @type WatcherData[]
- */
-const watchers = [
-  {
-    deploymentName: 'triton-converter',
-    jobType: 'convert'
-  },
-  {
-    deploymentName: 'triton-downloader',
-    jobType: 'newMedia'
-  }
-]
 
 /**
  * @type {Map<String, Watcher>}
@@ -87,10 +79,9 @@ const publishStatus = async status => {
 /**
  * Creates and manages a queue watcher
  * @param {WatcherData} w watcher to setup
- * @param {kube} kube kubernetes client
+ * @param {Kube} kube kubernetes client
  * @param {kue.Queue} queue kue queue object
  * @param {JobQueue} jobQueue job queue
- *
  * @returns {String} watcherId
  */
 const createWatcher = async function (w, kube, queue) {
@@ -108,7 +99,8 @@ const createWatcher = async function (w, kube, queue) {
     if (inactiveJobs !== 0 && canScale) {
       await jobQueue.create({
         op: 'scaleUp',
-        watcher: watcher.id
+        watcher: watcher.id,
+        queue: w.jobType
       })
     }
   })
@@ -117,13 +109,11 @@ const createWatcher = async function (w, kube, queue) {
     const activeJobs = active.length
     // process new stuff
     const replicas = await kube.getReplicas(w.deploymentName)
-    wlog.info(`activeJobs=${activeJobs},replicas=${replicas}`)
     if (replicas > activeJobs) {
-      logger.debug('there are more replicas than active jobs, scaleDown')
-
       await jobQueue.create({
         op: 'scaleDown',
-        watcher: watcher.id
+        watcher: watcher.id,
+        queue: w.jobType
       })
     }
   })
@@ -142,18 +132,30 @@ const init = async () => {
   })
 
   logger.info('loading kubespec')
-  const kube = await require('./lib/kube')(config)
+  const kube = await Kube(config)
 
   /**
    * Operations to be run on events.
    */
   const ops = {
     scaleUp: async function (data) {
+      logger.info(`executing scaleUp on '%o'`, data)
+
+      const watcher = watcherTable.get(data.watcher)
+      const deployment = watcher.deployment
+
+      await kube.scaleUp(deployment)
       publishStatus({
         event: 'scaleUp'
       })
     },
     scaleDown: async function (data) {
+      logger.info(`executing scaleDown on '%o'`, data)
+
+      const watcher = watcherTable.get(data.watcher)
+      const deployment = watcher.deployment
+
+      await kube.scaleDown(deployment)
       publishStatus({
         event: 'scaleDown'
       })
@@ -168,20 +170,28 @@ const init = async () => {
       if (!ops[job.data.op]) {
         logger.warn('failed to execute job:', job.data.op, 'on watcher', job.data.watcher)
       }
+
+      await ops[job.data.op](job.data)
+      await jobQueue.finish(job.id)
     }
 
     await printStatus()
   }
 
-  // check every 5 seconds
-  setInterval(poll, 5000)
+  // check every 20 seconds
+  setInterval(poll, 1000 * 20)
 
-  for (const w of watchers) {
-    const watcherId = await createWatcher(w, kube, queue)
-    logger.info(`created watcher '${watcherId}' from '%o'`, w)
+  const k8sWatchers = await kube._client.apis['tritonjs.com'].v1.namespaces('default').autoscalerwatchers.get()
+  for (const w of k8sWatchers.body.items) {
+    if (!w.spec || !w.spec.deploymentName || !w.spec.jobType) {
+      logger.warn('Skipping invalid watcher', w.metadata.name)
+    }
+    const watcherId = await createWatcher(w.spec, kube, queue)
+    logger.info(`created watcher '${watcherId}' from '%o'`, w.spec)
   }
 
   queue.watchStuckJobs()
+  jobQueue.startWatcher()
 
   logger.info('initialized')
 
